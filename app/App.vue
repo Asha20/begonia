dc6b8c48c936c651d70983a65fd1dbfb4de08154
Vukasin Stepanovic <vukasinstepanovic@gmail.com>
2017-03-05 14:51:37 +0100

    Add note list

--- app/App.vue
+++ app/App.vue
@@ -1,7 +1,59 @@
 <template>
-  
+  <div class="app container-fluid">
+    <div class="row">
+      <div class="col-sm-3">
+        <note-list
+          :notes="notes">
+        </note-list>
+      </div>
+
+      <div class="col-sm-offset-1 col-sm-8">
+        
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-  
+<script>
+  import Events from "./events";
+  import Firebase from "firebase";
+  import NoteList from "./components/NoteList.vue"
+
+  let config = {
+    apiKey: "AIzaSyCzplRModOK-ir_ybndG10S6croPq1I8fU",
+    authDomain: "begonia-cbe77.firebaseapp.com",
+    databaseURL: "https://begonia-cbe77.firebaseio.com",
+    storageBucket: "begonia-cbe77.appspot.com",
+    messagingSenderId: "656500064171"
+  };
+
+  Firebase.initializeApp(config);
+  const database = Firebase.database();
+
+
+
+  export default {
+    components: {
+      NoteList
+    },
+
+    data() {
+      return {
+        notes: [
+          {name: "Note1"},
+          {name: "Note2"}
+        ]
+      }
+    },
+
+    created() {
+      Events.on("noteSelected", index => console.log(index));
+    }
+  };
+</script>
+
+<style scoped>
+  .app {
+    padding-top: 30px;
+  }
 </style>
---
+++ app/components/NoteList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="list-group">
+    <button v-for="(note, index) in notes" @click="selectNote(index)" class="list-group-item" :class="[index === selected ? 'active' : '']">
+      {{ note.name }}
+    </button>
+  </div>
+</template>
+
+<script>
+  import Events from "../events";
+
+  export default {
+    props: ["notes"],
+
+    data() {
+      return {
+        selected: 0
+      }
+    },
+
+    methods: {
+      selectNote(index) {
+        this.selected = index;
+        Events.emit("noteSelected", index);
+      }
+    }
+  }
+</script>
